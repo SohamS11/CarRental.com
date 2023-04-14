a463cf463569aaa558a337df306cce45cc6aa3c8
Abhay Panchal <abpanchal951357@gmail.com>
2023-04-14 20:35:31 +0530

    router setup done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./component/Footer/Footer";
 import Navbar from "./component/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
