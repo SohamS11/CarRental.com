80d25cd625304005c0d2d53b584d246d2dc6abd6
Abhay Panchal <abpanchal951357@gmail.com>
2023-04-15 11:41:24 +0530

    MainDash Done!

--- src/component/MainDash/MainDash.jsx
+++ src/component/MainDash/MainDash.jsx
@@ -1,8 +1,23 @@
 import React from "react";
-import './MainDash.scss'
+import "./MainDash.scss";
 
 const MainDash = () => {
-  return <div className="MainDash">MainDash</div>;
+  return (
+    <div className="MainDash">
+      <div className="maindash-content">
+        <div className="info-section">
+          <div className="detail-section">
+            <span>Luxury Limo Hire</span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non,
+              minima dolores quasi asperiores
+            </p>
+          </div>
+          <button>Book Fleet</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainDash;
